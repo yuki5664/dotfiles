bcf2c5898ccf24ec99e72c052d321eaa9a5145b9
yuki tanaka <monkey.yebisu.7777@gmail.com>
2021-12-27 10:06:51 +0900

    Add brew install list

---
+++ setup/install/brew.zsh
@@ -0,0 +1,46 @@
+util::info 'install brew formulas...'
+
+formulas=(
+	anyenv
+	cmake
+	fzf
+	git
+	git-flow-avh
+	gcc
+	neovim
+	openssl
+	ripgrep
+	sqlite
+	tmux
+	tree-sitter
+	zsh-completions
+	zsh-git-prompt
+)
+
+cask_formulas=(
+	google-chrome
+	slack
+	google-drive
+	sourcetreee
+	deepl
+	visual-studio-code
+	discord
+	graphiql
+	zoom
+	docker
+	iterm2
+	notion
+	kindle
+)
+
+brew upgrate
+
+for formula in ${formulas[@]}; do
+  brew install ${formula}
+done
+
+for cask_formula in ${cask_formulas[@]}; do
+  brew install __cask ${formula}
+done
+
+brew cleanup
