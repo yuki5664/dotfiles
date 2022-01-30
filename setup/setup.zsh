ac40852878b8402ea8fdb0aa1078fe350bd3a097
tanaka5664 <monkey.yebisu.7777@gmail.com>
2022-01-30 21:36:54 +0900

    Add setting create config links

--- setup/setup.zsh
+++ setup/setup.zsh
@@ -1,14 +1,28 @@
 #!/bin/bash -e
 
-cd $HOME/dotfiles
+cd ${HOME}/dotfiles
 
 IGNORE_PATTERN="^\.(git|travis)"
 
 echo "Create dotfile links."
      for dotfile in *; do
 	  if [[ ${dotfile} != 'setup' ]] && [[ ${dotfile} != 'config' ]] && [[ ${dotfile} != 'vscode' ]] && [[ ${dotfile} != 'README.md' ]]; then
-        	[[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-        	ln -snfv "$(pwd)/$dotfile" "$HOME/.$dotfile"
+        	[[ ${dotfile} =~ ${IGNORE_PATTERN} ]] && continue
+        	ln -snfv "$(pwd)/$dotfile" "${HOME}/.${dotfile}"
           fi
      done
+echo "Success"
+
+echo "Create config links."
+# deploy config
+if [[ ! -d ${HOME}/.config ]]; then
+  mkdir ${HOME}/.config
+fi
+cd config
+for name in *; do
+  if [[ -L ${CONFIG_HOME:-$HOME/.config}/$name ]]; then
+    unlink ${CONFIG_HOME:-$HOME/.config}/$name
+  fi
+  ln -sfv ${PWD}/${name} ${CONFIG_HOME:-$HOME/.config}/${name}
+done
 echo "Success"
--- zshenv
+++ zshenv
@@ -6,3 +6,5 @@
 export LANG=ja_JP.UTF-8
 export LC_ALL=ja_JP.UTF-8
 export TZ=Asia/Tokyo
+
+export CONFIG_HOME=${HOME}/.config
