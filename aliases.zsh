562e19e2c45e0238e3b6fe8d58689a26d2f2d375
tanaka5664 <monkey.yebisu.7777@gmail.com>
2022-06-06 15:23:28 +0900

    Add aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,13 +1,30 @@
-# ls 
-# -F でファイル種別の表示
-# -G で色を付ける (GNU/Linux での "--color=auto" と等価)
+# ==============================
+# General
+# ==============================
+alias vim='nvim'
+alias reload='source ~/.zshenv && source ~/.zshrc'
+
+# ==============================
+# list
+# ==============================
 alias ls="ls -FG"
 alias la="ls -a"
 alias ll="ls -l"
 alias lla="ls -la"
 
-## doceker
+# ==============================
+# doceker
+# ==============================
 alias lzd='lazydocker'
 alias dc='docker compose'
 
-alias pkill= 'kill -9 $(lsof -t -i:$1)'
+# ==============================
+# base64
+# ==============================
+alias encode='function __encode(){ echo -n "$1" | base64 }; __encode'
+alias decode='function __decode(){ echo -n "$1" | base64 -d }; __decode'
+
+# ==============================
+# gcloud
+# ==============================
+alias gcpp='gcloud config set project $(gcloud projects list | fzf | awk "{print \$1}")'
