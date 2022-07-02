f152a576cd7915e70dc3c361fc2963a99106c975
tanaka5664 <monkey.yebisu.7777@gmail.com>
2022-07-03 08:38:03 +0900

    Add: kubernetes aliace

--- aliases.zsh
+++ aliases.zsh
@@ -28,3 +28,16 @@ alias decode='function __decode(){ echo -n "$1" | base64 -d }; __decode'
 # gcloud
 # ==============================
 alias gcpp='gcloud config set project $(gcloud projects list | fzf | awk "{print \$1}")'
+
+# ------------------------------
+# kubectl
+# ------------------------------
+alias -g KP='$(kubectl get pods | fzf | awk "{print \$1}")'
+alias -g KD='$(kubectl get deploy | fzf | awk "{print \$1}")'
+alias -g KS='$(kubectl get svc | fzf | awk "{print \$1}")'
+alias -g KI='$(kubectl get ing | fzf | awk "{print \$1}")'
+alias -g KJ='$(kubectl get job | fzf | awk "{print \$1}")'
+alias -g KA='$(kubectl get all | awk "! /NAME/" | fzf | awk "{print \$1}")'
+alias kubectle='kubectl exec -it KP'
+alias kubectll='kubectl stern $(kubectl get deploy | fzf | awk "{print \$1}")'
+alias kubectlo='kubectl get KA -o yaml'
