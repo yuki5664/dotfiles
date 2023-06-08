712fb9b4012ccad4da12931c32a1a21e6a0b1ae0
tanaka5664 <monkey.yebisu.7777@gmail.com>
2023-06-08 11:01:26 +0900

    Add: wezterm pane setting

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,39 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = {
   -- colors
   color_scheme = '3024 Night',
+}
+
+
+config.keys = {
+  -- This will create a new split and run the `top` program inside it
+  {
+    key = '-',
+    mods = 'ALT',
+    action = act.SplitPane {
+      direction = 'Down',
+      size = { Percent = 30 },
+    },
+  },
+  {
+    key = '=',
+    mods = 'ALT',
+    action = act.SplitPane {
+      direction = 'Right',
+      size = { Percent = 50 },
+    },
+  },
+  -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
+  { key = '8', mods = 'CTRL', action = act.PaneSelect },
+  -- show the pane selection mode, but have it swap the active and selected panes
+  {
+    key = '0',
+    mods = 'CTRL',
+    action = act.PaneSelect {
+      mode = 'SwapWithActive',
+    },
+  },
 }
 return config
